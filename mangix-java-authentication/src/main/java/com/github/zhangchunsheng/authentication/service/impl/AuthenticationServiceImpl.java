3dc78697b1537b8552653459f7f8156e35460603
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-04-12 15:32:54 +0800

    1. add login-url api

--- mangix-java-authentication/src/main/java/com/github/zhangchunsheng/authentication/service/impl/AuthenticationServiceImpl.java
+++ mangix-java-authentication/src/main/java/com/github/zhangchunsheng/authentication/service/impl/AuthenticationServiceImpl.java
@@ -5,7 +5,7 @@ import com.github.zhangchunsheng.authentication.bean.result.LoginUrlResult;
 import com.github.zhangchunsheng.authentication.service.AuthenticationService;
 import me.zhangchunsheng.mangix.common.constant.MangixConstants;
 import me.zhangchunsheng.mangix.common.exception.MangixException;
-import me.zhangchunsheng.mangix.common.service.impl.MangixServiceOkHttpImpl;
+import me.zhangchunsheng.mangix.common.service.impl.MangixServiceApacheHttpImpl;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -18,7 +18,7 @@ import java.net.URLEncoder;
  *
  * @author <a href="https://github.com/zhangchunsheng">Chunsheng Zhang</a>
  */
-public class AuthenticationServiceImpl extends MangixServiceOkHttpImpl implements AuthenticationService {
+public class AuthenticationServiceImpl extends MangixServiceApacheHttpImpl implements AuthenticationService {
     @Override
     public LoginUrlResult getLoginUrl(String username, String next) throws MangixException {
         try {
